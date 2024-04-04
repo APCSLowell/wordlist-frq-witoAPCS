1460ec845d125b3e39bde2aef0a1cf0f7270afc4
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-04 11:44:03 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -13,7 +13,7 @@ public class WordList
     /* to be implemented in part (a) */
     public int balls = 0;
     for(int i = 0; i < myList.size(); i++){
-     if(myList.get(i).length == len) balls++;
+     if(myList.get(i).length() == len) balls++;
     }
     return balls;
   }
