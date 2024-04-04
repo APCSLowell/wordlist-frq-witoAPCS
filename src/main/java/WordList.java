0396e66cdf89a7fd8288f32306fde77d538d455b
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-04 12:27:21 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -24,10 +24,9 @@ public class WordList
      for(int i = 0;  i < myList.size() ; i++){
        if(myList.get(i).length() == len){
          myList.remove(i);
+         i--;
        }
-         else{
-         i--; 
-       }
+
     }
   }
 }
