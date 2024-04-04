2279ec71534d2e1e6d9da8dcaff36180e9d1606e
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-04 12:21:57 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -21,9 +21,11 @@ public class WordList
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-     for(int i = myList.size()-1;  i >= 0 ; i--){
-       if(myList.get(i).length() == len) myList.remove(i);
-
+     for(int i = 0;  i < myList.size() ; i++){
+       if(myList.get(i).length() == len){
+         myList.remove(i);
+         i++; 
+       }
     }
   }
 }
